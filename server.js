fe547a2dd87903c43a30272a27e6a2cba8ba532a
osama <engrsyedusamaakhtar@gmail.com>
2024-08-14 17:45:06 +0100

    some changes

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://dev.encoders.com.pk",
+  origin: "http://dev-enc.online",
   credentials: true,
 }));
 
